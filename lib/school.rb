55facf13e2e8899a864fc7dd558cf65e1e40991a
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-28 15:43:43 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+  attr_accessor :roster
+
+  def initialize(school_name)
+    @name = school_name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    grades = @roster.keys.sort
+    sorted_school = {}
+    grades.each do |grade|
+      students = @roster[grade].sort
+      sorted_school[grade] = students
+    end
+    sorted_school
+  end
+
+end
